056645359cd207b67e68879197928b9cb2041246
Nicholas Powers <njpowers7915@gmail.com>
2018-05-12 19:23:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,5 @@
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
-  @@executed_transfers = []
   
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -34,7 +33,6 @@ class Transfer
       @sender.deposit(sender_amount)
       @receiver.deposit(receiver_amount)
       @status = "complete"
-      @@executed_transfers << self
     elsif is_sender_valid? == false
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
