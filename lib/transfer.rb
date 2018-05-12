7d35cc28dd3202695057e26fceb4414e426ea3c9
Nicholas Powers <njpowers7915@gmail.com>
2018-05-12 19:02:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,8 @@ class Transfer
   def sender_not_valid?
     if @sender.valid? != true
       @status = "rejected"
-    
+      "Transaction rejected. Please check your account balance."
+    end
   end
   
   def execute_transaction
